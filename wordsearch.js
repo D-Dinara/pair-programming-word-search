bfe4a6e7dd02e826cd00a9810cfefce6f04798b2
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-16 17:14:44 -0500

    refactor loop to be a separate function and conditionally check horizontal then vertical for presence of word

--- wordsearch.js
+++ wordsearch.js
@@ -1,29 +1,46 @@
+// map over each sub-array and join the letters
 const mapAndJoinLetters = (arr) => {
   return arr.map(ls => ls.join(''));
 };
 
-const wordSearch = (letters, word) => {
-  if (letters.length === 0) return false;
-  const horizontalJoin = mapAndJoinLetters(letters);
-  for (const l of horizontalJoin) {
-    if (l.includes(word)) return true;
+// transpose the array when checking vertically
+const transposeArray = (arr) => {
+  const reversed = [];
+
+  for (let i = 0; i < arr[0].length; i++) {
+    reversed[i] = [];
+    for (let j = 0; j < arr.length; j++) {
+      reversed[i].push(arr[j][i]);
+    }
   }
+  return reversed;
+};
 
-  let reveresedMatrix = [];
-
-  for (let i = 0; i < letters[0].length; i++) {
-    reveresedMatrix[i] = [];
-    for (let j = 0; j < letters.length; j++) {
-      reveresedMatrix[i].push(letters[j][i]);
+// check the joined sub arrays for the required word
+const checkIfWordIncluded = (joinedArr, word) => {
+  for (const subString of joinedArr) {
+    if (subString.includes(word)) {
+      return true;
     }
   }
 
-  const verticalJoin = mapAndJoinLetters(reveresedMatrix);
-  for (const l of verticalJoin) {
-    if (l.includes(word)) return true;
-  }
-
   return false;
+};
+
+const wordSearch = (letters, word) => {
+  // if array is empty, return false
+  if (letters.length === 0) return false;
+
+  // map and join letters horizontally
+  const horizontalJoin = mapAndJoinLetters(letters);
+  // transpose, and map and join letters vertically
+  const transposed = transposeArray(letters);
+  const verticalJoin = mapAndJoinLetters(transposed);
+
+  const checkHorizontal = checkIfWordIncluded(horizontalJoin, word);
+
+  // if horizontal check is false, check vertically. Otherwise return true.
+  return !checkHorizontal ? checkIfWordIncluded(verticalJoin, word) : true;
 };
 
 module.exports = wordSearch;
